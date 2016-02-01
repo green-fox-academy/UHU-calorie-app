de6e40ada2b7ae8b67c47c6d4bcaa2ff66478fe8
André Zsolt <andrezsolt71@gmail.com>
2016-02-01 15:30:52 +0100

    jshint into gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,16 @@
+'use strict';
+
 // grab our gulp packages
 var gulp  = require('gulp'),
     gutil = require('gulp-util');
+var jshint = require('gulp-jshint');
 
 // create a default task and just log a message
 gulp.task('default', function() {
-  return gutil.log('Gulp is running!')
+  return gutil.log('Gulp is running!');
 });
+
+gulp.src(['*.js'])
+  .pipe(jshint())
+  .pipe(jshint.reporter('jshint-stylish'))
+  .pipe(jshint.reporter('fail'));
