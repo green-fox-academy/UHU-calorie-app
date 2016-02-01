7d0169e6b9c6277d3ae7215860d149ff8a87e0ae
Bertalan Eva <eva0bertalan@gmail.com>
2016-02-01 17:35:20 +0100

    add ava to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@
 
 var gulp = require('gulp'); 
 var jshint = require('gulp-jshint');
+var ava = require('gulp-ava');
+
 
 gulp.task('jshint', function() {
   gulp.src('./*.js')
@@ -13,4 +15,9 @@ gulp.task('watch', function() {
   gulp.watch('./*.js', ['jshint']);
 });
 
+gulp.task('default', () => {
+	return gulp.src('test.js')
+		// gulp-ava needs filepaths so you can't have any plugins before it 
+		.pipe(ava());
+});
 gulp.task('default', ['watch']);
