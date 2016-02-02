7176127e4e533f2b06617b499b419d299312e542
kristoftombacz <tombacz.kristof@gmail.com>
2016-02-02 11:25:10 +0100

    invoke the cleardb connection

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 'use strict';
 
 var express = require('express');
+var mysql = require('./connection.js');
 var app = express();
+
+mysql.conn.connect();
 
 app.use(express.static('public'));
 app.listen(process.env.PORT || 3000);
