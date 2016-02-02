bca92516d3cfd01780fe94988c1b1f67e33a1c6e
kristoftombacz <tombacz.kristof@gmail.com>
2016-02-02 12:16:04 +0100

    tweaking the server

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 'use strict';
 
 var express = require('express');
-var mysql = require('./connection.js');
+//var mysql = require('./connection.js');
 var app = express();
-var url = 'https://uhu-systems-calorie.herokuapp.com';
 
 app.use(express.static('public'));
 app.listen(process.env.PORT || 3000);
