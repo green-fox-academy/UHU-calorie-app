2acbc7f0f615eaef4ca09e127ab99c415dbecbbe
André Zsolt <andrezsolt71@gmail.com>
2016-02-02 14:21:52 +0100

    database query working

--- connection.js
+++ connection.js
@@ -2,22 +2,15 @@
 
 var mysql = require('mysql');
 
-var connection = mysql.createConnection({
+var db_config = {
   host: 'eu-cdbr-west-01.cleardb.com',
   user: 'be2be2ab917e7e',
   password: '95dd4052',
   database: 'heroku_e0c3a986f86f1ae',
   timezone: 'utc'
-});
+};
 
-connection.connect(function(err) {
-  console.log(err.code); // 'ECONNREFUSED'
-  console.log(err.fatal); // true
-});
-
-
-
-
+var connection = mysql.createConnection(db_config);
 
 module.exports = {
   conn: connection
