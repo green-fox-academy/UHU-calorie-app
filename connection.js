0ffc98c2c07b0d3d0c4ada89128164cf09db2275
André Zsolt <andrezsolt71@gmail.com>
2016-02-02 13:45:09 +0100

    connection error handling

--- connection.js
+++ connection.js
@@ -1,6 +1,7 @@
 'use strict';
 
 var mysql = require('mysql');
+
 var connection = mysql.createConnection({
   host: 'eu-cdbr-west-01.cleardb.com',
   user: 'be2be2ab917e7e',
@@ -8,7 +9,14 @@ var connection = mysql.createConnection({
   database: 'heroku_e0c3a986f86f1ae'
 });
 
-connection.connect();
+connection.connect(function(err) {
+  console.log(err.code); // 'ECONNREFUSED'
+  console.log(err.fatal); // true
+});
+
+
+
+
 
 module.exports = {
   conn: connection
