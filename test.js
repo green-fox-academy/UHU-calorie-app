1247ded55ba9d889823f3cef8a6d4a386aea97d2
Attila Vaczy <attila.vaczy@yahoo.com>
2016-02-02 15:34:50 +0100

    new testcases in items.js

--- test.js
+++ test.js
@@ -3,6 +3,14 @@
 import test from 'ava';
 import items from './items';
 
+var mysql = require('./connection.js');
+
 test(t => {
   t.is(typeof items.list, 'function');
+  t.is(typeof items.del, 'function');
+  t.is(typeof items.add, 'function');
+});
+
+test(t => {
+  t.same(deleteMeals())
 });
