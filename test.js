e93bf5e5914f3f24db544ba923ce0f0723f16f6b
Bertalan Eva <eva0bertalan@gmail.com>
2016-02-02 17:46:42 +0100

    test

--- test.js
+++ test.js
@@ -1,18 +1,21 @@
 'use strict';
 
 import test from 'ava';
-import items from './items';
-
+// import items from './items';
 test(t => {
-  t.is(typeof items.list, 'function');
-  t.is(typeof items.del, 'function');
-  t.is(typeof items.add, 'function');
+    t.same([1, 2], [1, 2]);
 });
 
+// test(t => {
+//   t.is(typeof items.list, 'function');
+//   t.is(typeof items.del, 'function');
+//   t.is(typeof items.add, 'function');
+// });
 
-test.cb(t => {
-  t.is(items.del(131, function(err, res) {
-    t.is(res.affectedRows, 0);
-    t.end();
-  }));
-});
+
+// test.cb(t => {
+//   t.is(items.del(131, function(err, res) {
+//     t.is(res.affectedRows, 0);
+//     t.end();
+//   }));
+// });
