7a836a38a822b3fda5bb1bdbebffa98304b135ed
kristoftombacz <tombacz.kristof@gmail.com>
2016-02-02 17:34:42 +0100

    deleting another err msg

--- items.js
+++ items.js
@@ -4,19 +4,19 @@ var mysql = require('./connection.js');
 
 function listMeals(cb){
   mysql.conn.query('SELECT * FROM meal;', function(err, res){
-    cb(err, res);
+    cb(res);
   });
 }
 
 function addMeals(meal, cb){
   mysql.conn.query('INSERT INTO meal SET ?;', meal,  function(err, res){
-    cb(err, res);
+    cb(res);
   });
 }
 
 function deleteMeals(id, cb){
   mysql.conn.query('DELETE FROM meal WHERE meal_id = ?;', id,  function(err, res){
-    cb(err, res);
+    cb(res);
   });
 }
 
