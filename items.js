f3360638666be5a20727fb38bcacfd65dff92da8
Bertalan Eva <eva0bertalan@gmail.com>
2016-02-02 17:19:56 +0100

    items.js is finally back

---
+++ items.js
@@ -0,0 +1,38 @@
+ 'use strict';
+ 
+ var mysql = require('./connection.js');
+ 
+ function listMeals(cb){
+   mysql.conn.query('SELECT * FROM meal;', function(err, res){
+     cb(res);
+   });
+ }
+ 
+ function addMeals(meal, cb){
+   mysql.conn.query('INSERT INTO meal SET ?;', meal,  function(err, res){
+     cb(res);
+   });
+ }
+ 
+ function deleteMeals(id, cb){
+   mysql.conn.query('DELETE FROM meal WHERE meal_id = ?;', id,  function(err, res){
+     cb(err, res);
+   });
+  }
+  
+  var test = {
+ /* jshint ignore:start */
+    meal_name: 'viccaaaa',
+ /* jshint ignore:end */
+    calories: 666,
+  };
+  
+ addMeals(test, function(err, res){console.log(res);});
+ listMeals(function(err, res){console.log(res);});
+ deleteMeals(131, function(err, res){console.log(res);});
+ 
+ module.exports = {
+   add: addMeals,
+   del: deleteMeals,
+   list: listMeals
+ };
