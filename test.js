5224f679748ba881771ad8dfc7d8052369bcea65
Bertalan Eva <eva0bertalan@gmail.com>
2016-02-02 17:59:07 +0100

    test chaos

--- test.js
+++ test.js
@@ -1,21 +1,18 @@
 'use strict';
 
 import test from 'ava';
-// import items from './items';
+import items from './items';
+
 test(t => {
-    t.same([1, 2], [1, 2]);
+  t.is(typeof items.list, 'function');
+  t.is(typeof items.del, 'function');
+  t.is(typeof items.add, 'function');
 });
 
-// test(t => {
-//   t.is(typeof items.list, 'function');
-//   t.is(typeof items.del, 'function');
-//   t.is(typeof items.add, 'function');
-// });
 
-
-// test.cb(t => {
-//   t.is(items.del(131, function(err, res) {
-//     t.is(res.affectedRows, 0);
-//     t.end();
-//   }));
-// });
+test.cb(t => {
+  t.is(items.del(131, function(err, res) {
+    t.is(res.affectedRows, 0);
+    t.end();
+  }));
+});
