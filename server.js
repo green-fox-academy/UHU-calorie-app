8e28912e63777d36c82b95a7bf04deee5936b04d
Bertalan Eva <eva0bertalan@gmail.com>
2016-02-02 14:20:52 +0100

    go back to previous version

--- server.js
+++ server.js
@@ -1,26 +1,14 @@
 'use strict';
 
 var express = require('express');
-// var mysql = require('./connection.js');
-var mysql = require('mysql');
+var mysql = require('./connection.js');
 var app = express();
-
-var connection = mysql.createConnection({
-  host: 'eu-cdbr-west-01.cleardb.com',
-  user: 'be2be2ab917e7e',
-  password: '95dd4052',
-  database: 'heroku_e0c3a986f86f1ae',
-  timezone: 'utc'
-});
 
 app.use(express.static('public'));
 app.listen(process.env.PORT || 3000);
 
 app.get('/', function(req, res) {
-	connection.query('SELECT * FROM meal', function(err, result) {
-		if (err) throw err;
-		res.send('Hello');
-	});
+  res.send('Hello');
 });
 
 app.get('/hello', function(req, res) {
