854d2668d605ef6ef321a7c3aea802e7599d0ab0
kristoftombacz <tombacz.kristof@gmail.com>
2016-02-02 11:48:30 +0100

    changing back everything

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var express = require('express');
-//var mysql = require('./connection.js');
+var mysql = require('./connection.js');
 var app = express();
-//var url = 'https://uhu-systems-calorie.herokuapp.com/';
+var url = 'https://uhu-systems-calorie.herokuapp.com/';
 
-//app.use(express.static('public'));
+app.use(express.static('public'));
 app.listen(process.env.PORT || 3000);
 
-app.get('/', function(res,req) {
+app.get('/hello', function(res,req) {
   res.send('Hello World');
 });
