5c62f376ae229b5c515f013c1988f59e0697b065
kristoftombacz <tombacz.kristof@gmail.com>
2016-02-02 11:14:23 +0100

    adding mysql connection and server test

---
+++ connection.js
@@ -0,0 +1,15 @@
+'use strict';
+
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+  host: 'eu-cdbr-west-01.cleardb.com',
+  user: 'be2be2ab917e7e',
+  password: '95dd4052',
+  database: 'heroku_e0c3a986f86f1ae'
+});
+
+connection.connect();
+
+module.exports = {
+  conn: connection
+}
--- server.js
+++ server.js
@@ -4,4 +4,8 @@ var express = require('express');
 var app = express();
 
 app.use(express.static('public'));
-app.listen(process.env.PORT);
+app.listen(process.env.PORT || 3000);
+
+app.get('/hello', function(res,req) {
+  console.log('Hello World' + res + req);
+});
