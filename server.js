263f218099335895c6099b7ca5febff880313a80
kristoftombacz <tombacz.kristof@gmail.com>
2016-02-02 11:57:02 +0100

    changing my failes

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 'use strict';
 
 var express = require('express');
-//var mysql = require('./connection.js');
+var mysql = require('./connection.js');
 var app = express();
 var url = 'https://uhu-systems-calorie.herokuapp.com';
 
 app.use(express.static('public'));
 app.listen(process.env.PORT || 3000);
 
-app.get(url + '/hello', function(res,req) {
+app.get(url + '/hello', function(req, res) {
   res.send('Hello World');
 });
