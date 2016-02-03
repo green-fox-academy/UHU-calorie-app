d200122c0987a3529b02403106f2becc92186b97
Attila Vaczy <attila.vaczy@yahoo.com>
2016-02-03 11:02:17 +0100

    test file invitations renamed

--- test.js
+++ test.js
@@ -1,17 +1,17 @@
 'use strict';
 
 import test from 'ava';
-import items from './items';
+import meals from './meals';
 
 test(t => {
-  t.is(typeof items.list, 'function');
-  t.is(typeof items.del, 'function');
-  t.is(typeof items.add, 'function');
+  t.is(typeof meals.list, 'function');
+  t.is(typeof meals.del, 'function');
+  t.is(typeof meals.add, 'function');
 });
 
 
 test.cb(t => {
-  t.is(items.del(131, function(err, res) {
+  t.is(meals.del(131, function(err, res) {
     t.is(res.affectedRows, 0);
     t.end();
   }));
