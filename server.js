a0183b90dd71975d15416dfc68f7a3186fc70a9c
André Zsolt <andrezsolt71@gmail.com>
2016-02-03 16:53:24 +0100

    server add function try

--- server.js
+++ server.js
@@ -30,7 +30,8 @@ app.delete('/meals/:id', function (req, res){
 app.post('/meals', function (req, res) {
   meals.add(req.body, function(err, result) {
       	res.json({
-    	status: 'ok'
+    	status: ok,
+      result: result
   	});
   });
 });
