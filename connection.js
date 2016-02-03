ca7f03a9c05ba885b349acdd85302170b840c07b
kristoftombacz <tombacz.kristof@gmail.com>
2016-02-03 16:15:31 +0100

    added enviroment variable and localhost

--- connection.js
+++ connection.js
@@ -3,14 +3,14 @@
 var mysql = require('mysql');
 
 var dbConfig = {
-  host: 'eu-cdbr-west-01.cleardb.com',
-  user: 'be2be2ab917e7e',
-  password: '95dd4052',
-  database: 'heroku_e0c3a986f86f1ae',
+  host: 'localhost',
+  user: 'test',
+  password: 'test',
+  database: 'database',
   timezone: 'utc'
 };
 
-var connection = mysql.createConnection(dbConfig);
+var connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL || dbConfig);
 connection.connect();
 
 // function handleDisconnection() {
