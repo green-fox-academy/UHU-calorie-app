961ce0a386b6a3c4aec8f7693e91c8d14dc6660d
Bertalan Eva <eva0bertalan@gmail.com>
2016-02-10 17:31:37 +0100

    serv

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 
 var main = require('./main.js');
 var defaultPort = 3000;
+<<<<<<< Updated upstream
 var app = main.serverFunction(connectionCreator());
 
 function serverStartLog(){
@@ -25,3 +26,51 @@ function connectionCreator(){
 }
 
 app.listen(process.env.PORT || defaultPort, serverStartLog);
+=======
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+app.listen(process.env.PORT || defaultPort);
+
+var dbConfig = {
+  host: 'localhost',
+  user: 'root',
+  password: '',
+  database: 'calories',
+  timezone: 'utc'
+};
+
+var connection = mysql.createConnection(process.env.CLEARDB_DATABASE_URL || dbConfig);
+
+connection.connect();
+
+var meals = new Meal(connection);
+
+app.get('/meals', function(req, res) {
+  meals.list(function(err, result){
+    if (err){
+      res.json({status: 'not exists'});
+    } else {
+      res.json(result);
+    }
+  });
+});
+
+app.delete('/meals/:id', function (req, res){
+  meals.del(req.params.id, function(err, result) {
+    if (err) {
+      res.json(result);
+    } else {
+      res.json({status: 'ok'});
+    }
+  });
+});
+
+app.post('/meals', function (req, res) {
+  meals.add(req.body, function(err, result) {
+    res.json({
+      result: result
+    });
+  });
+});
+>>>>>>> Stashed changes
