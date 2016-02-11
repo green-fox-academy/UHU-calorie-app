19acd55388e889a92652a1cda3451e12c07334cc
André Zsolt <andrezsolt71@gmail.com>
2016-02-11 11:26:30 +0100

    sass watch into gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,7 @@
 var gulp = require('gulp'); 
 var jshint = require('gulp-jshint');
 var ava = require('gulp-ava');
+var sass = require('gulp-sass');
 
 gulp.task('jshint', function() {
   gulp.src('./*.js')
@@ -16,9 +17,16 @@ gulp.task('test', function() {
   .pipe(ava());
 });
 
+gulp.task('sass', function () {
+  return gulp.src('./sass/**/*.scss')
+    .pipe(sass.sync().on('error', sass.logError))
+    .pipe(gulp.dest('./css'));
+});
+
 gulp.task('watch', function() {
   gulp.watch('./*.js', ['jshint']);
   gulp.watch('./*.js', ['test']);
+  gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
 gulp.task('ci', ['test', 'jshint']);
