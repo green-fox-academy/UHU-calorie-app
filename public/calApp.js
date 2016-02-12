bb2a70ac8d72ea5ee9d8cf4e9ba88c1da7103dbb
kristoftombacz <tombacz.kristof@gmail.com>
2016-02-12 12:39:22 +0100

    missing semicolons

--- public/calApp.js
+++ public/calApp.js
@@ -15,36 +15,36 @@ calApp.controller('MainController', function($rootScope, $scope, $http) {
     var config = {
       headers : {'Content-Type': 'application/json'}
     };
-    
+
     var clearInputFields = function() {
       $scope.addNewMeal.meal_name = '';
       $scope.addNewMeal.calories = 0;
-    }
-    
+    };
+
     var succesCbPost = function(response) {
       $scope.meals.push(newMeal);
-    } 
-    
+      console.log(response);
+    };
+
     var errorCbPost = function(response) {
       $scope.error = response.data;
-    }
-    
+    };
+
     $http.post(localURL, newMeal, config).then(succesCbPost,errorCbPost);
     clearInputFields();
   };
-  
+
   var succesCbGet = function (response){
     $scope.meals = response.data;
-  }
-  
+  };
   var errorCbGet= function (response){
     $scope.error = response.data;
-  }
+  };
 
   $scope.getAll = function() {
     $http.get(localURL).then( succesCbGet, errorCbGet);
   };
-  
+
   $scope.addNewMeal.date = new Date();
   $scope.getAll();
 });
